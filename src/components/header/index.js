c135f231501b81ef5d9fe13e763140133b795e3c
diegomrz <diegomrz@outlook.com>
2020-03-12 22:54:02 -0300

    #012 - Header style adjust.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,10 @@ import './style.css';
 class Header extends Component {
   render() {
     return (
-      <header id='fbheader'>Facebook</header>
+      <header id='fbheader'>
+        <h3>facebook</h3>
+        <p>sair</p>
+      </header>
     );
   }
 
